28dd5d5450bdae1d9ad10d5dc9656eef0f2b5237
Patricia Johnson <pjohns25@uwo.ca>
2019-09-15 17:52:01 -0400

    clean up

--- log_functions.py
+++ log_functions.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 
 def plot_pair(dat,target):
-     fig=plt.figure()
+     fig = plt.figure()
      plt.gray()
      fig.add_subplot(1,2,1)
      plt.imshow(dat[0,0,:,:])
      fig.add_subplot(1,2,2)
      plt.imshow(target[0,0,:,:])
      plt.show(block=False)
+
 def print_stats(loss, epoch, i, running_loss=0.0):
      running_loss += loss.item()
      

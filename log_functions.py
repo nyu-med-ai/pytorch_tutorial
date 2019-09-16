83c9329765fba9853bd91d3b289d09ec6d14e469
Matthew Muckley <matt.muckley@gmail.com>
2019-09-16 09:16:15 -0400

    Added plot_trio.

--- log_functions.py
+++ log_functions.py
@@ -1,20 +1,32 @@
 import matplotlib.pyplot as plt
 
-def plot_pair(dat,target):
-     fig = plt.figure()
-     plt.gray()
-     fig.add_subplot(1,2,1)
-     plt.imshow(dat[0,0,:,:])
-     fig.add_subplot(1,2,2)
-     plt.imshow(target[0,0,:,:])
-     plt.show(block=False)
+
+def plot_pair(dat, target):
+    fig = plt.figure()
+    plt.gray()
+    fig.add_subplot(1, 2, 1)
+    plt.imshow(dat[0, 0, :, :])
+    fig.add_subplot(1, 2, 2)
+    plt.imshow(target[0, 0, :, :])
+    plt.show(block=False)
+
+
+def plot_trio(dat, est, target):
+    fig = plt.figure()
+    plt.gray()
+    fig.add_subplot(1, 3, 1)
+    plt.imshow(dat[0, 0, :, :])
+    fig.add_subplot(1, 3, 2)
+    plt.imshow(est[0, 0, :, :])
+    fig.add_subplot(1, 3, 3)
+    plt.imshow(target[0, 0, :, :])
+    plt.show(block=False)
+
 
 def print_stats(loss, epoch, i, running_loss=0.0):
-     running_loss += loss.item()
-     
-     if i % 5 == 4:    # print every 2000 mini-batches
-          print('[%d, %5d] loss: %.3f' %
-          (epoch + 1, i + 1, running_loss / 5))
-          running_loss = 0.0
+    running_loss += loss.item()
 
-
+    if i % 5 == 4:    # print every 2000 mini-batches
+        print('[%d, %5d] loss: %.3f' %
+              (epoch + 1, i + 1, running_loss / 5))
+        running_loss = 0.0
